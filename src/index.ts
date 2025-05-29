023ce2d6f2bd47c6b4dc945c6e73be5d8e08b4e8
xskutsu <xsku@protonmail.com>
2025-05-29 06:08:18 -0500

    Much progress

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
-export class Entity {
+export class Vector2 {
 	public x: number;
 	public y: number;
 	constructor(x: number, y: number) {
 		this.x = x;
 		this.y = y;
 	}
+}
+
+export class Entity {
+	public position: Vector2;
+	public velocity: Vector2;
+	public radius: number;
+	public friction: number;
+	constructor(x: number, y: number, radius: number) {
+		this.position = new Vector2(x, y);
+		this.velocity = new Vector2(0, 0);
+		this.radius = radius;
+		this.friction = 0.95;
+	}
 
 	public tick(deltaTime: number): void {
-
+		const decay = Math.pow(this.friction, deltaTime);
+		this.velocity.x *= decay;
+		this.velocity.y *= decay;
+		this.position.x += this.velocity.x * deltaTime;
+		this.position.y += this.velocity.y * deltaTime;
 	}
 }
 
@@ -37,16 +54,16 @@ export class Scene {
 	}
 
 	public destroy() {
-		if (this.lastTickTime === -1) {
+		if (this.lastTickTime !== -1) {
 			clearInterval(this.intervalIndex);
 		}
 	}
 
 	public tick(): void {
 		let deltaTime = 1;
-		if (this.lastTickTime === -1) {
+		if (this.lastTickTime !== -1) {
 			const currentTime = performance.now();
-			deltaTime = Math.min((currentTime - this.lastTickTime) / 1000, 0.1);
+			deltaTime = (currentTime - this.lastTickTime) / 10;
 			this.lastTickTime = currentTime;
 		}
 		for (const entity of this.entities.values()) {
