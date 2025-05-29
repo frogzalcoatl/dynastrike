fcc2919c94a191d6fe64585090dbf1a0cec2ee72
xskutsu <xsku@protonmail.com>
2025-05-29 06:36:33 -0500

    Implement AABB

--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,55 @@ export class Vector2 {
 	}
 }
 
+export interface AABB {
+	minX: number;
+	minY: number;
+	maxX: number;
+	maxY: number;
+}
+
 export class Entity {
 	public position: Vector2;
 	public velocity: Vector2;
 	public radius: number;
 	public friction: number;
+	public aabb: AABB;
 	constructor(x: number, y: number, radius: number) {
 		this.position = new Vector2(x, y);
 		this.velocity = new Vector2(0, 0);
 		this.radius = radius;
 		this.friction = 0.95;
+		this.aabb = {
+			minX: this.position.x - radius,
+			minY: this.position.y - radius,
+			maxX: this.position.x + radius,
+			maxY: this.position.y + radius
+		};
+	}
+
+	public moveTo(x: number, y: number): void {
+		this.position.x = x;
+		this.position.y = y;
+		this.aabb.minX = x - this.radius;
+		this.aabb.minY = y - this.radius;
+		this.aabb.maxX = x + this.radius;
+		this.aabb.maxY = y + this.radius;
+	}
+
+	public moveBy(x: number, y: number): void {
+		this.position.x += x;
+		this.position.y += y;
+		this.aabb.minX += x;
+		this.aabb.minY += y;
+		this.aabb.maxX += x;
+		this.aabb.maxY += y;
 	}
 
 	public tick(deltaTime: number): void {
 		const decay = Math.pow(this.friction, deltaTime);
 		this.velocity.x *= decay;
 		this.velocity.y *= decay;
-		this.position.x += this.velocity.x * deltaTime;
-		this.position.y += this.velocity.y * deltaTime;
+		this.moveBy(this.velocity.x * deltaTime, this.velocity.y * deltaTime);
 	}
 }
 
