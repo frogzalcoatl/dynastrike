670633d2621ea76896c58303769d26c19260432b
xskutsu <xsku@protonmail.com>
2025-06-01 10:10:08 -0500

    Start work on polygons

--- src/entity/entity.ts
+++ src/entity/entity.ts
@@ -1,4 +1,5 @@
-import { Box } from "../geometry/box";
+import { Box, computeBox } from "../geometry/box";
+import { Circle, computeMEC } from "../geometry/mec";
 import { Vector2 } from "../geometry/vector";
 
 const TAU = Math.PI * 2;
@@ -10,10 +11,11 @@ export class Entity {
 	public radius: number;
 	public mass: number;
 	public isStatic: boolean;
-	public box: Box;
 	public angle: number;
 	public angularVelocity: number;
-	constructor(x: number, y: number, radius: number) {
+	public points: number[] | null;
+	public box: Box;
+	constructor(x: number, y: number, radius: number, points: number[] | null = null) {
 		this.index = entityIndexTicker++;
 		this.position = {
 			x: x,
@@ -26,14 +28,55 @@ export class Entity {
 		this.radius = radius;
 		this.mass = 1;
 		this.isStatic = false;
-		this.box = {
-			minX: this.position.x - radius,
-			minY: this.position.y - radius,
-			maxX: this.position.x + radius,
-			maxY: this.position.y + radius
-		};
 		this.angle = 0;
 		this.angularVelocity = 0;
+		if (points === null) {
+			this.points = null;
+			this.box = {
+				minX: this.position.x - radius,
+				minY: this.position.y - radius,
+				maxX: this.position.x + radius,
+				maxY: this.position.y + radius
+			};
+		} else {
+			this.points = [];
+			const circle: Circle = computeMEC(points.slice());
+			const factor: number = circle.radius < 1e-9 ? 0 : this.radius / circle.radius;
+			for (let i = 0; i < points.length; i += 2) {
+				this.points.push(this.position.x + (points[i] - circle.x) * factor, this.position.y + (points[i + 1] - circle.y) * factor);
+			}
+			this.box = computeBox(this.points);
+		}
+	}
+
+	public updateBox(): void {
+		if (this.points === null) {
+			this.box.minX = this.position.x - this.radius;
+			this.box.minY = this.position.y - this.radius;
+			this.box.maxX = this.position.x + this.radius;
+			this.box.maxY = this.position.y + this.radius;
+		} else {
+			this.box.minX = this.points[0];
+			this.box.minY = this.points[1];
+			this.box.maxX = this.points[0];
+			this.box.maxY = this.points[1];
+			for (let i = 2; i < this.points.length; i += 2) {
+				const pointX = this.points[i];
+				const pointY = this.points[i + 1];
+				if (this.box.minX > pointX) {
+					this.box.minX = pointX;
+				}
+				if (this.box.minY > pointY) {
+					this.box.minY = pointY;
+				}
+				if (this.box.maxX < pointX) {
+					this.box.maxX = pointX;
+				}
+				if (this.box.maxY < pointY) {
+					this.box.maxY = pointY;
+				}
+			}
+		}
 	}
 
 	public moveBy(x: number, y: number): void {
@@ -43,6 +86,12 @@ export class Entity {
 		this.box.minY += y;
 		this.box.maxX += x;
 		this.box.maxY += y;
+		if (this.points !== null) {
+			for (let i = 0; i < this.points.length; i += 2) {
+				this.points[i] += x;
+				this.points[i + 1] += y;
+			}
+		}
 	}
 
 	public scaleBy(factor: number): void {
@@ -55,6 +104,17 @@ export class Entity {
 
 	public turnBy(radians: number): void {
 		this.angle = (((this.angle + radians) % TAU) + TAU) % TAU;
+		if (this.points !== null) {
+			const cos = Math.cos(radians);
+			const sin = Math.sin(radians);
+			for (let i = 0; i < this.points.length; i += 2) {
+				const relativeX = this.points[i] - this.position.x;
+				const relativeY = this.points[i + 1] - this.position.y;
+				this.points[i] = this.position.x + relativeX * cos - relativeY * sin;
+				this.points[i + 1] = this.position.y + relativeX * sin + relativeY * cos;
+			}
+			this.updateBox();
+		}
 	}
 
 	public tick(): void {
--- src/geometry/box.ts
+++ src/geometry/box.ts
@@ -19,10 +19,18 @@ export function computeBox(points: number[]): Box {
 	for (let i = 2; i < points.length; i += 2) {
 		const pointX = points[i];
 		const pointY = points[i + 1];
-		if (box.minX > pointX) box.minX = pointX;
-		if (box.minY > pointY) box.minY = pointY;
-		if (box.maxX < pointX) box.maxX = pointX;
-		if (box.maxY < pointY) box.maxY = pointY;
+		if (box.minX > pointX) {
+			box.minX = pointX;
+		}
+		if (box.minY > pointY) {
+			box.minY = pointY;
+		}
+		if (box.maxX < pointX) {
+			box.maxX = pointX;
+		}
+		if (box.maxY < pointY) {
+			box.maxY = pointY;
+		}
 	}
 	return box;
 }
